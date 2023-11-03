5c03a0e1de4f3a1b531fa729ae3cb53005034606
EvgeniyLednev <lednev.fortech@gmail.com>
2023-11-03 17:23:22 +0300

    - Добавлена возможность проверки баланса пользователя

--- src/telegram-bot/scenes/merchant.scene.ts
+++ src/telegram-bot/scenes/merchant.scene.ts
@@ -1,8 +1,14 @@
-import { Context, Scene, SceneEnter } from 'nestjs-telegraf';
+import { Context, Scene, SceneEnter, Hears } from 'nestjs-telegraf';
 import { TrongridService } from 'src/providers/trongrid/trongrid.service';
 import { UsersService } from 'src/users/users.service';
 import { Scenes } from 'telegraf';
 import { BotScenes } from '../constants';
+import { SceneContext } from 'telegraf/typings/scenes';
+import { Update } from 'telegraf/typings/core/types/typegram';
+
+enum MerchantCommands {
+  getMyBalance = 'Узнать баланс',
+}
 
 @Scene(BotScenes.merchant)
 export class MerchantScene {
@@ -17,10 +23,20 @@ export class MerchantScene {
 
     const { id } = ctx.message?.from;
     const user = await this.usersService.getUserByTelegramId(id);
-    if (user?.telegramUser?.username) {
-      return `[${user?.role}] Добро пожаловать, ${user?.telegramUser.username}`;
-    } else {
-      return `[${user?.role}] Добро пожаловать!`;
-    }
+    await ctx.reply(`[${user?.role}] Добро пожаловать!`, {
+      reply_markup: {
+        keyboard: [[{ text: MerchantCommands.getMyBalance }]],
+      },
+    });
+  }
+
+  @Hears(MerchantCommands.getMyBalance)
+  async getMyBalance(
+    @Context() ctx: SceneContext & { update: Update.CallbackQueryUpdate },
+  ) {
+    const id = ctx.message?.from?.id;
+    if (!id) return 'Не удалось получить баланс';
+    const user = await this.usersService.getUserByTelegramId(id);
+    await ctx.reply(`Баланс: ${user?.balance.usdt} USDT`);
   }
 }
--- src/telegram-bot/scenes/trader.scene.ts
+++ src/telegram-bot/scenes/trader.scene.ts
@@ -1,8 +1,14 @@
-import { Context, Scene, SceneEnter } from 'nestjs-telegraf';
+import { Context, Scene, SceneEnter, Hears } from 'nestjs-telegraf';
 import { TrongridService } from 'src/providers/trongrid/trongrid.service';
 import { UsersService } from 'src/users/users.service';
 import { Scenes } from 'telegraf';
 import { BotScenes } from '../constants';
+import { SceneContext } from 'telegraf/typings/scenes';
+import { Update } from 'telegraf/typings/core/types/typegram';
+
+enum TraderCommands {
+  getMyBalance = 'Узнать баланс',
+}
 
 @Scene(BotScenes.trader)
 export class TraderScene {
@@ -17,12 +23,20 @@ export class TraderScene {
 
     const { id } = ctx.message?.from;
     const user = await this.usersService.getUserByTelegramId(id);
-    if (user?.telegramUser?.username) {
-      await ctx.reply(
-        `[${user?.role}] Добро пожаловать, ${user?.telegramUser.username}`,
-      );
-    } else {
-      await ctx.reply(`[${user?.role}] Добро пожаловать!`);
-    }
+    await ctx.reply(`[${user?.role}] Добро пожаловать!`, {
+      reply_markup: {
+        keyboard: [[{ text: TraderCommands.getMyBalance }]],
+      },
+    });
+  }
+
+  @Hears(TraderCommands.getMyBalance)
+  async getMyBalance(
+    @Context() ctx: SceneContext & { update: Update.CallbackQueryUpdate },
+  ) {
+    const id = ctx.message?.from?.id;
+    if (!id) return 'Не удалось получить баланс';
+    const user = await this.usersService.getUserByTelegramId(id);
+    await ctx.reply(`Баланс: ${user?.balance.usdt} USDT`);
   }
 }
