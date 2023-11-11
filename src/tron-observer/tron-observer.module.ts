a6b7c400ad42e5326df34fb41fabda339625fb99
clackgot <clackgot@gmail.com>
2023-11-11 18:03:47 +0300

    - Обновлен TronObserver. Теперь там используется TronModule, вместо TrongridModule

--- src/tron-observer/tron-observer.module.ts
+++ src/tron-observer/tron-observer.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TronObserverService } from './tron-observer.service';
 import { UsersModule } from 'src/users/users.module';
 import { TrongridModule } from 'src/providers/trongrid/trongrid.module';
+import { TronModule } from 'src/tron/tron.module';
 
 @Module({
-  imports: [UsersModule, TrongridModule],
+  imports: [UsersModule, TronModule],
   providers: [TronObserverService],
 })
 export class TronObserverModule {}
--- src/tron-observer/tron-observer.service.ts
+++ src/tron-observer/tron-observer.service.ts
@@ -2,23 +2,33 @@ import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
 import { UserRole } from 'src/models/user.model';
 import { TrongridService } from 'src/providers/trongrid/trongrid.service';
+import { TronAccountInfo } from 'src/providers/trongrid/types';
+import { TronService } from 'src/tron/tron.service';
 import { UsersService } from 'src/users/users.service';
+
+type UserBalanceInfo = TronAccountInfo & { username: string | number };
 
 @Injectable()
 export class TronObserverService {
   private logger = new Logger(this.constructor.name);
   constructor(
     private readonly usersService: UsersService,
-    private readonly trongridService: TrongridService,
+    private readonly tronService: TronService,
   ) {}
   // @Cron(CronExpression.EVERY_5_SECONDS)
   async observeTronBalances() {
     const users = await this.usersService.getAllUsers();
-    const accounts = users.map((u) => u.tronAccount.address);
-    const promises = accounts.map((a) =>
-      this.trongridService.getTronAccountInfoByAddress(a),
-    );
-    const balances = await Promise.all(promises);
+    const userBalanceList: Promise<UserBalanceInfo>[] = users.map(async (u) => {
+      const info = await this.tronService.getTronAccountInfoByAddress(
+        u.tronAccount.address,
+      );
+      return {
+        ...info,
+        username: u.telegramUser.username || u.telegramUser.id,
+      };
+    });
+
+    const balances = await Promise.all(userBalanceList);
     console.log(balances);
   }
 }
